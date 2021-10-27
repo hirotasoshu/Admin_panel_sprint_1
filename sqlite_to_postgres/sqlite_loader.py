b465f7b68d44af00e657119d595d54f8ad34aa2c
Maksim Zayakin <rasetsu@list.ru>
2021-10-27 18:19:47 +0500

    :sparkles: Add sqlite loader

---
+++ sqlite_to_postgres/sqlite_loader.py
@@ -0,0 +1,29 @@
+from sqlite3 import Connection
+from typing import Iterator, Type
+
+from models import Data, FilmWork, Genre, GenreFilmWork, Person, PersonFilmWork, T
+
+
+class SQLiteLoader:
+    def __init__(self, connection: Connection) -> None:
+        self.connection = connection
+        self.cursor = self.connection.cursor()
+
+    def _load_table(self, table_name: str, table_model: Type[T]) -> Iterator[T]:
+        for data in self.cursor.execute(f"SELECT * from {table_name}"):
+            yield table_model(*data)
+
+    def load_movies(self) -> Data:
+        movies = {}
+        movies["film_work"] = self._load_table("film_work", FilmWork)
+        movies["genre"] = self._load_table("genre", Genre)
+        movies["genre_film_work"] = self._load_table("genre_film_work", GenreFilmWork)
+        movies["person"] = self._load_table("person", Person)
+        movies["person_film_work"] = self._load_table(
+            "person_film_work", PersonFilmWork
+        )
+
+        return movies
+
+    def __del__(self) -> None:
+        self.cursor.close()
