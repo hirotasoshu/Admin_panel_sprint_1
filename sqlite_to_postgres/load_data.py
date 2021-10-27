10786d07c2d7eceb8731611898259ccdc26a0c55
Maksim Zayakin <rasetsu@list.ru>
2021-10-27 18:24:11 +0500

    :necktie: Uncomment load_from_sqlite logic

--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -4,17 +4,21 @@ import sqlite3
 
 import psycopg2
 from dotenv import load_dotenv
+from postgres_saver import PostgresSaver
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import DictCursor
+from sqlite_loader import SQLiteLoader
 
 
-def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
+def load_from_sqlite(
+    connection: sqlite3.Connection, pg_conn: _connection, chunk_size: int = 1000
+) -> None:
     """Основной метод загрузки данных из SQLite в Postgres"""
-    # postgres_saver = PostgresSaver(pg_conn)
-    # sqlite_loader = SQLiteLoader(connection)
+    postgres_saver = PostgresSaver(pg_conn, chunk_size)
+    sqlite_loader = SQLiteLoader(connection)
 
-    # data = sqlite_loader.load_movies()
-    # postgres_saver.save_all_data(data)
+    data = sqlite_loader.load_movies()
+    postgres_saver.save_all_data(data)
 
 
 if __name__ == "__main__":
@@ -23,6 +27,7 @@ if __name__ == "__main__":
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.INFO,
     )
+
     dsl = {
         "dbname": os.getenv("DB_NAME", "movies"),
         "user": os.getenv("POSTGRES_USER", "postgres"),
@@ -31,6 +36,7 @@ if __name__ == "__main__":
         "port": int(os.getenv("DB_PORT", 5432)),
         "options": "-c search_path=content",
     }
+
     with sqlite3.connect("db.sqlite") as sqlite_conn, psycopg2.connect(
         **dsl, cursor_factory=DictCursor
     ) as pg_conn:
